a03f80f90e54076d090492b2bc885bb470a537c3
Mehran Dabestani <mehran.dabes@gmail.com>
2022-10-20 13:03:09 +0330

    Add wallet service

---
+++ domain/wallet/service/service.go
@@ -0,0 +1,93 @@
+package service
+
+import (
+	"context"
+	"wallet-manager/domain/constants"
+	"wallet-manager/domain/wallet/dto"
+	"wallet-manager/domain/wallet/repository"
+	"wallet-manager/domain/wallet/utils"
+)
+
+type IWalletService interface {
+	Create(ctx context.Context, userID int64) (wallet *dto.Wallet, err error)
+	AddFund(ctx context.Context, ID int64, fund uint64) (wallet *dto.Wallet, err error)
+	SubtractFund(ctx context.Context, ID int64, fund uint64) (wallet *dto.Wallet, err error)
+	GetByID(ctx context.Context, ID int64) (wallet *dto.Wallet, err error)
+	GetByUserID(ctx context.Context, userID int64) (wallet *dto.Wallet, err error)
+}
+
+type WalletService struct {
+	repository repository.IWalletRepository
+}
+
+func NewWalletService(repository repository.IWalletRepository) *WalletService {
+	return &WalletService{repository: repository}
+}
+
+func (w *WalletService) Create(ctx context.Context, userID int64) (wallet *dto.Wallet, err error) {
+	walletEntity, err := w.repository.Create(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet = utils.WalletDTOFromEntity(walletEntity)
+
+	return wallet, nil
+}
+
+func (w *WalletService) AddFund(ctx context.Context, ID int64, fund uint64) (wallet *dto.Wallet, err error) {
+	if err := w.repository.AddFund(ctx, ID, fund); err != nil {
+		return nil, err
+	}
+
+	walletEntity, err := w.repository.GetByID(ctx, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet = utils.WalletDTOFromEntity(walletEntity)
+
+	return wallet, nil
+}
+
+func (w *WalletService) SubtractFund(ctx context.Context, ID int64, fund uint64) (wallet *dto.Wallet, err error) {
+	walletEntity, err := w.repository.GetByID(ctx, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if walletEntity.Balance-fund < 0 {
+		return nil, constants.ErrBalanceNotSufficient
+	}
+
+	if err := w.repository.SubtractFund(ctx, ID, fund); err != nil {
+		return nil, err
+	}
+
+	walletEntity.Balance -= fund
+	wallet = utils.WalletDTOFromEntity(walletEntity)
+
+	return wallet, nil
+}
+
+func (w *WalletService) GetByID(ctx context.Context, ID int64) (wallet *dto.Wallet, err error) {
+	walletEntity, err := w.repository.GetByID(ctx, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet = utils.WalletDTOFromEntity(walletEntity)
+
+	return wallet, nil
+}
+
+func (w *WalletService) GetByUserID(ctx context.Context, userID int64) (wallet *dto.Wallet, err error) {
+	walletEntity, err := w.repository.GetByUserID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet = utils.WalletDTOFromEntity(walletEntity)
+
+	return wallet, nil
+}
