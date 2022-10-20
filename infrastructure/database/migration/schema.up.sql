cfe45785194ce02f2ec9c32379e5094dd9117dca
Mehran Dabestani <mehran.dabes@gmail.com>
2022-10-20 13:11:53 +0330

    Fix create table sql

--- infrastructure/database/migration/schema.up.sql
+++ infrastructure/database/migration/schema.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS wallets (
     id INT(32) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    balance INT(32) NOT NULL DEAFULT 0,
+    balance INT(32) NOT NULL DEFAULT 0,
     user_id INT(32) NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp
