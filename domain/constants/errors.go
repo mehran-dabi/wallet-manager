865e7fe5ee378675500817afe9b5916a06e67b7c
Mehran Dabestani <mehran.dabes@gmail.com>
2022-10-20 13:37:21 +0330

    Add new error

--- domain/constants/errors.go
+++ domain/constants/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrNotFound             = errors.New("record not found")
 	ErrBalanceNotSufficient = errors.New("balance is not sufficient")
+	ErrUserAlreadyHasWallet = errors.New("user already has a wallet")
 )
